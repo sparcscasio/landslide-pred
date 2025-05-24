b8951e27bb291ac2584a18a6d1642d0cf3ad2cdc
sparcscasio <162997498+sparcscasio@users.noreply.github.com>
2025-05-25 06:45:38 +0900

    landslide

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-# How to run this project
+# How to run this project
 
-Flask server (Google colab) : https://colab.research.google.com/drive/1SE7KX33Yq420xssk9NwnGYXP30T0p7XS?authuser=0#scrollTo=iVP_AWoz-fi-
-1) 'city data.csv' & 'city data_ann.csv' 업로드
+Flask server (Google colab) : https://colab.research.google.com/drive/1ox23xK641R9zeH5F4euZ-dAn57iHMfmd#scrollTo=9IFRxyf-gXEn
+1) 'data1.csv' & 'data2.csv' 업로드
 2) 모든 셀 실행하기
 3) 마지막에서 2번쨰 셀 실행 결과로부터 ngrok tunnel 링크 얻기 (https://{id}.ngrok-free.app/ 형식)
 4) App.jsx 10번 line 수정 -> "https://{id}.ngrok-free.app/calc"
 5) terminal에서 git add . -> git commit -m "new commit" -> git push -u origin main -> npm run deploy
-6) https://sparcscasio.github.io/heat-exhaustion/ 재접속
+6) https://sparcscasio.github.io/landslide-pred/ 재접속
--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,19 @@
 import React, { useState } from "react";
+import './App.css'; 
 
 const App = () => {
   const [loading, setLoading] = useState(false);
   const [result, setResult] = useState(0);
-  const [r1, setR1] = useState(1.0);
-  const [r2, setR2] = useState(1.0);
-  const [r3, setR3] = useState(1.0);
-  const [r4, setR4] = useState(1.0);
-  const [r5, setR5] = useState(1.0);
-  const [r6, setR6] = useState(1.0);
-  const [r7, setR7] = useState(1.0);
-  const [r8, setR8] = useState(1.0);
-  const [r9, setR9] = useState(1.0);
-  const [r10, setR10] = useState(1.0);
-  const [r11, setR11] = useState(1.0);
-  const [r12, setR12] = useState(1.0);
+  const [reg, setReg] = useState('강원특별자치도');
+  const [year, setYear] = useState(2024);
 
-
-  const minRes = 0.4205267131328583;
-  const maxRes = 26.24397850036621;
-
-  const apiUrl = "https://e907-34-172-72-200.ngrok-free.app/calc";
+  const apiUrl = "https://5cc9-34-106-52-67.ngrok-free.app/predict";
 
   const handleCalculate = async () => {
 
-    const vision = (r1+r2) * 50;
-    const auditory = (r3+r4) * 50;
-    const physical = (r5+r6) * 50;
-    const emotional = (r8+r7) * 50;
-    const self_ = (r9+r10) * 50;
-    const communication = (r11+r12) * 50;
-
-    console.log(r9, r10);
-
-    console.log(vision, auditory, physical, emotional, self_, communication);
-    
     setLoading(true);
     try {
-      const response = await fetch(`${apiUrl}?vision=${vision}&auditory=${auditory}&physical=${physical}&emotional=${emotional}&self=${self_}&communication=${communication}`, {
+      const response = await fetch(`${apiUrl}/2025/${reg}`, {
         headers: {
           'ngrok-skip-browser-warning': 'true'  // ngrok 경고 페이지를 생략
         }
@@ -50,16 +26,7 @@ const App = () => {
       const data = JSON.parse(textResponse);
       console.log(data);
       const res = data.result;
-      const tuned = (res-minRes) / (maxRes-minRes);
-      if (tuned < 0) {
-        setResult(0);
-      } else {
-        if (tuned > 1) {
-          setResult(100);
-        } else {
-          setResult(tuned * 100);
-        }
-      }
+      setResult(res);
     } catch (error) {
       console.error("Error calling the API:", error);
     } finally {
@@ -69,197 +36,67 @@ const App = () => {
   
 
   return (
-    <div style={{padding: "15px"}}>
-      <h1>노인의 삶의 질 예측</h1>
+    <div style={{display: "flex", alignItems: "center", justifyContent: "center",flexDirection: "column", height: "100vh", gap: "20px"}}>
+      <h1>산사태 발생 회수 예측</h1>
       <div style={{ display: "flex", flexDirection: "column", gap: "5px" }}>
-        {/* 시각적 제한 */}
-        <label>
-          Q1: 어두운 곳이나 밝은 햇빛 아래에서 글씨를 읽는 것이 어렵다고 느낀 적이 있나요?
-          
-          <select
-            value={r1}
-            onChange={(e) => setR1(e.target.value)}
-          >
-            <option value={1.0}>매우 그렇다</option>
-            <option value={2.0}>그렇다</option>
-            <option value={3.0}>보통이다</option>
-            <option value={4.0}>아니다</option>
-            <option value={5.0}>전혀 아니다</option>
-          </select>
+        <label style={{fontSize: "20px", fontWeight: "700", color: "#107F4F"}}>
+          <div style={{display: "flex", flexDirection: "row", gap: "20px", alignItems: "center"}}>
+            지역 선택
+            <select
+              value={reg}
+              onChange={(e) => setReg(e.target.value)}
+              style={{
+                padding: "3px",
+                fontSize: "16px",
+                borderRadius: "3px",
+                fontWeight: 400,
+              }}
+            >
+              <option value={'강원특별자치도'}>강원특별자치도</option>
+              <option value={'경기도'}>경기도</option>
+              <option value={'경상남도'}>경상남도</option>
+              <option value={'경상북도'}>경상북도</option>
+              <option value={'광주광역시'}>광주광역시</option>
+              <option value={'대구광역시'}>대구광역시</option>
+              <option value={'대전광역시'}>대전광역시</option>
+              <option value={'부산광역시'}>부산광역시</option>
+              <option value={'서울특별시'}>서울특별시</option>
+              <option value={'세종특별자치시'}>세종특별자치시</option>
+              <option value={'울산광역시'}>울산광역시</option>
+              <option value={'인천광역시'}>인천광역시</option>
+              <option value={'전라남도'}>전라남도</option>
+              <option value={'전북특별자치도'}>전북특별자치도</option>
+              <option value={'제주특별자치도'}>제주특별자치도</option>
+              <option value={'충청남도'}>충청남도</option>
+              <option value={'충청북도'}>충청북도</option>
+            </select>
+          </div>
+        </label>
+        
+        <label style={{fontSize: "20px", fontWeight: "700", color: "#107F4F"}}>
+        <div style={{display: "flex", flexDirection: "row", gap: "20px", alignItems: "center"}}>
+          연도
+          <input type="number" value={year} onChange={(e) => setYear(e.target.value)}               
+              style={{
+                padding: "3px",
+                fontSize: "16px",
+                fontWeight: 400,
+              }}/>
+          </div>
         </label>
         <br />
-        <label>
-          Q2: TV를 볼 때 화면 속 글씨나 사람의 얼굴을 분명하게 구별하기 어렵나요?
-          <select
-            value={r2}
-            onChange={(e) => setR2(e.target.value)}
-          >
-            <option value={1.0}>매우 그렇다</option>
-            <option value={2.0}>그렇다</option>
-            <option value={3.0}>보통이다</option>
-            <option value={4.0}>아니다</option>
-            <option value={5.0}>전혀 아니다</option>
-          </select>
-        </label>
-        <br />
-        {/* 신체적 제한 */}
-        <label>
-          Q3: 혼자서 의자에서 일어나거나 앉을 때 어려움을 느낀 적이 있나요?
-          <select
-            value={r3}
-            onChange={(e) => setR3(e.target.value)}
-          >
-            <option value={1.0}>매우 그렇다</option>
-            <option value={2.0}>그렇다</option>
-            <option value={3.0}>보통이다</option>
-            <option value={4.0}>아니다</option>
-            <option value={5.0}>전혀 아니다</option>
-          </select>
-        </label>
-        <br />
-        <label>
-          Q4: 무거운 물건을 들거나 높은 곳에 있는 물건을 잡는 것이 힘들다고 느낀 적이 있나요?
-          <select
-            value={r4}
-            onChange={(e) => setR4(e.target.value)}
-          >
-            <option value={1.0}>매우 그렇다</option>
-            <option value={2.0}>그렇다</option>
-            <option value={3.0}>보통이다</option>
-            <option value={4.0}>아니다</option>
-            <option value={5.0}>전혀 아니다</option>
-          </select>
-        </label>
-        <br />
-        {/* 청각적 제한 */}
-        <label>
-          Q5: 여러 사람이 함께 이야기할 때 대화를 따라가기 어려운 경우가 있나요?
-          <select
-            value={r5}
-            onChange={(e) => setR5(e.target.value)}
-          >
-            <option value={1.0}>매우 그렇다</option>
-            <option value={2.0}>그렇다</option>
-            <option value={3.0}>보통이다</option>
-            <option value={4.0}>아니다</option>
-            <option value={5.0}>전혀 아니다</option>
-          </select>
-        </label>
-        <br />
-        <label>
-          Q6: TV나 라디오의 볼륨을 다른 사람들보다 더 크게 해야 잘 들리나요?
-          <select
-            value={r6}
-            onChange={(e) => setR6(e.target.value)}
-          >
-            <option value={1.0}>매우 그렇다</option>
-            <option value={2.0}>그렇다</option>
-            <option value={3.0}>보통이다</option>
-            <option value={4.0}>아니다</option>
-            <option value={5.0}>전혀 아니다</option>
-          </select>
-        </label>
-        <br />
-        {/* 자기관리 제한 */}
-        <label>
-          Q7: 혼자서 세수하거나 양치하는 것이 어렵다고 느낀 적이 있나요?
-          <select
-            value={r7}
-            onChange={(e) => setR7(e.target.value)}
-          >
-            <option value={1.0}>매우 그렇다</option>
-            <option value={2.0}>그렇다</option>
-            <option value={3.0}>보통이다</option>
-            <option value={4.0}>아니다</option>
-            <option value={5.0}>전혀 아니다</option>
-          </select>
-        </label>
-        <br />
-        <label>
-          Q8: 옷을 입거나 벗는 데 도움을 받아야 할 때가 있나요?
-          <select
-            value={r8}
-            onChange={(e) => setR8(e.target.value)}
-          >
-            <option value={1.0}>매우 그렇다</option>
-            <option value={2.0}>그렇다</option>
-            <option value={3.0}>보통이다</option>
-            <option value={4.0}>아니다</option>
-            <option value={5.0}>전혀 아니다</option>
-          </select>
-        </label>
-        <br />
-        {/* 정서적 만족도 */}
-        <label>
-          Q9: 하루 대부분을 기분 좋고 만족스럽게 보낸다고 느끼시나요?
-          <select
-            value={r9}
-            onChange={(e) => setR9(e.target.value)}
-          >
-            <option value={1.0}>매우 그렇다</option>
-            <option value={2.0}>그렇다</option>
-            <option value={3.0}>보통이다</option>
-            <option value={4.0}>아니다</option>
-            <option value={5.0}>전혀 아니다</option>
-          </select>
-        </label>
-        <br />
-        <label>
-          Q10: 가족이나 친구들과의 관계에서 정서적으로 충분한 지지를 받고 있다고 생각하시나요?
-          <select
-            value={r10}
-            onChange={(e) => setR10(e.target.value)}
-          >
-            <option value={1.0}>매우 그렇다</option>
-            <option value={2.0}>그렇다</option>
-            <option value={3.0}>보통이다</option>
-            <option value={4.0}>아니다</option>
-            <option value={5.0}>전혀 아니다</option>
-          </select>
-        </label>
-        <br />
-        {/* 의사소통 제한 */}
-        <label>
-          Q11: 전화 통화나 대면 대화에서 상대방이 말하는 내용을 이해하는 데 어려움을 느낀 적이 있나요?
-          <select
-            value={r11}
-            onChange={(e) => setR11(e.target.value)}
-          >
-            <option value={1.0}>매우 그렇다</option>
-            <option value={2.0}>그렇다</option>
-            <option value={3.0}>보통이다</option>
-            <option value={4.0}>아니다</option>
-            <option value={5.0}>전혀 아니다</option>
-          </select>
-        </label>
-        <br />
-        <label>
-          Q12: 자신의 의사를 정확하게 전달하는 것이 어렵거나 답답하게 느껴질 때가 있나요?
-          <select
-            value={r12}
-            onChange={(e) => setR12(e.target.value)}
-          >
-            <option value={1.0}>매우 그렇다</option>
-            <option value={2.0}>그렇다</option>
-            <option value={3.0}>보통이다</option>
-            <option value={4.0}>아니다</option>
-            <option value={5.0}>전혀 아니다</option>
-          </select>
-        </label>
-        <br />
-        <div>
-          <button onClick={handleCalculate} disabled={loading}>
+          <button onClick={handleCalculate} disabled={loading} className="calc-button">
             {loading ? "계산 중..." : "계산하기"}
           </button>
-        </div>
       </div>
 
       {result !== null && (
-        <div>
+        <div style={{display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"}}>
           <h2>
             결과:{" "}
-            {result}점
+            {result.toFixed(2)}회
           </h2>
+          {`${year}년에 ${reg}에서 예측되는 산사태 발생 횟수입니다.`}
         </div>
       )}
 
