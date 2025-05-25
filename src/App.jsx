3f3ce08603a82968e340d301c4e4957126593ab0
sparcscasio <162997498+sparcscasio@users.noreply.github.com>
2025-05-25 19:37:20 +0900

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,29 @@ const App = () => {
   const [result, setResult] = useState(0);
   const [reg, setReg] = useState('강원특별자치도');
   const [year, setYear] = useState(2024);
+  const [humd1, setHumd1] = useState(1.0);
+  const [humd2, setHumd2] = useState(1.0);
+  const [humd3, setHumd3] = useState(1.0);
 
-  const apiUrl = "https://5cc9-34-106-52-67.ngrok-free.app/predict";
+  const [temp1, setTemp1] = useState(1.0);
+  const [temp2, setTemp2] = useState(1.0);
+  const [temp3, setTemp3] = useState(1.0);
+
+  const [wind1, setWind1] = useState(1.0);
+  const [wind2, setWind2] = useState(1.0);
+  const [wind3, setWind3] = useState(1.0);
+
+  const [perp1, setPerp1] = useState(1.0);
+  const [perp2, setPerp2] = useState(1.0);
+  const [perp3, setPerp3] = useState(1.0);
+
+  const apiUrl = "https://be0e-35-231-81-149.ngrok-free.app//predict";
 
   const handleCalculate = async () => {
-
+    console.log(`${apiUrl}/${parseFloat(humd1).toFixed(2)}&${parseFloat(temp1).toFixed(2)}&${parseFloat(perp1).toFixed(2)}&${parseFloat(wind1).toFixed(2)}/${parseFloat(humd2).toFixed(2)}&${parseFloat(temp2).toFixed(2)}&${parseFloat(perp2).toFixed(2)}&${parseFloat(wind2).toFixed(2)}/${parseFloat(humd3).toFixed(2)}&${parseFloat(temp3).toFixed(2)}&${parseFloat(perp3).toFixed(2)}&${parseFloat(wind3).toFixed(2)}/${reg}`);
     setLoading(true);
     try {
-      const response = await fetch(`${apiUrl}/2025/${reg}`, {
+      const response = await fetch(`${apiUrl}/${parseFloat(humd1).toFixed(2)}&${parseFloat(temp1).toFixed(2)}&${parseFloat(perp1).toFixed(2)}&${parseFloat(wind1).toFixed(2)}/${parseFloat(humd2).toFixed(2)}&${parseFloat(temp2).toFixed(2)}&${parseFloat(perp2).toFixed(2)}&${parseFloat(wind2).toFixed(2)}/${parseFloat(humd3).toFixed(2)}&${parseFloat(temp3).toFixed(2)}&${parseFloat(perp3).toFixed(2)}&${parseFloat(wind3).toFixed(2)}/${reg}`, {
         headers: {
           'ngrok-skip-browser-warning': 'true'  // ngrok 경고 페이지를 생략
         }
@@ -84,6 +99,137 @@ const App = () => {
               }}/>
           </div>
         </label>
+        <label style={{fontSize: "20px", fontWeight: "700", color: "#107F4F"}}>
+        <div style={{display: "flex", flexDirection: "row", gap: "20px", alignItems: "center"}}>
+          {year-3}의 환경변수
+          <div style={{display: "flex", flexDirection: "row", gap: "5px", alignItems: "center", fontSize: "15px", color: "black"}}>
+            평균 온도
+          <input type="number" value={temp1} onChange={(e) => setTemp1(parseFloat(e.target.value))}               
+              style={{
+                padding: "3px",
+                fontSize: "16px",
+                fontWeight: 400,
+              }}/>
+              °C
+          </div>
+          <div style={{display: "flex", flexDirection: "row", gap: "5px", alignItems: "center", fontSize: "15px", color: "black"}}>
+            평균 습도
+          <input type="number" value={humd1} onChange={(e) => setHumd1(parseFloat(e.target.value))}               
+              style={{
+                padding: "3px",
+                fontSize: "16px",
+                fontWeight: 400,
+              }}/>
+              %rh
+          </div>
+          <div style={{display: "flex", flexDirection: "row", gap: "5px", alignItems: "center", fontSize: "15px", color: "black"}}>
+            연간 강수량
+          <input type="number" value={perp1} onChange={(e) => setPerp1(parseFloat(e.target.value))}               
+              style={{
+                padding: "3px",
+                fontSize: "16px",
+                fontWeight: 400,
+              }}/>
+              mm
+          </div>
+          <div style={{display: "flex", flexDirection: "row", gap: "5px", alignItems: "center", fontSize: "15px", color: "black"}}>
+            평균 풍속
+          <input type="number" value={wind1} onChange={(e) => setWind1(parseFloat(e.target.value))}               
+              style={{
+                padding: "3px",
+                fontSize: "16px",
+                fontWeight: 400,
+              }}/>
+              m/s
+          </div>
+          </div>
+          <div style={{display: "flex", flexDirection: "row", gap: "20px", alignItems: "center"}}>
+          {year-2}의 환경변수
+          <div style={{display: "flex", flexDirection: "row", gap: "5px", alignItems: "center", fontSize: "15px", color: "black"}}>
+            평균 온도
+          <input type="number" value={temp2} onChange={(e) => setTemp2(parseFloat(e.target.value))}               
+              style={{
+                padding: "3px",
+                fontSize: "16px",
+                fontWeight: 400,
+              }}/>
+              °C
+          </div>
+          <div style={{display: "flex", flexDirection: "row", gap: "5px", alignItems: "center", fontSize: "15px", color: "black"}}>
+            평균 습도
+          <input type="number" value={humd2} onChange={(e) => setHumd2(parseFloat(e.target.value))}               
+              style={{
+                padding: "3px",
+                fontSize: "16px",
+                fontWeight: 400,
+              }}/>
+              %rh
+          </div>
+          <div style={{display: "flex", flexDirection: "row", gap: "5px", alignItems: "center", fontSize: "15px", color: "black"}}>
+            연간 강수량
+          <input type="number" value={perp2} onChange={(e) => setPerp2(parseFloat(e.target.value))}               
+              style={{
+                padding: "3px",
+                fontSize: "16px",
+                fontWeight: 400,
+              }}/>
+              mm
+          </div>
+          <div style={{display: "flex", flexDirection: "row", gap: "5px", alignItems: "center", fontSize: "15px", color: "black"}}>
+            평균 풍속
+          <input type="number" value={wind2} onChange={(e) => setWind2(parseFloat(e.target.value))}               
+              style={{
+                padding: "3px",
+                fontSize: "16px",
+                fontWeight: 400,
+              }}/>
+              m/s
+          </div>
+          </div>
+          <div style={{display: "flex", flexDirection: "row", gap: "20px", alignItems: "center"}}>
+          {year-1}의 환경변수
+          <div style={{display: "flex", flexDirection: "row", gap: "5px", alignItems: "center", fontSize: "15px", color: "black"}}>
+            평균 온도
+          <input type="number" value={temp3} onChange={(e) => setTemp3(parseFloat(e.target.value))}               
+              style={{
+                padding: "3px",
+                fontSize: "16px",
+                fontWeight: 400,
+              }}/>
+              °C
+          </div>
+          <div style={{display: "flex", flexDirection: "row", gap: "5px", alignItems: "center", fontSize: "15px", color: "black"}}>
+            평균 습도
+          <input type="number" value={humd3} onChange={(e) => setHumd3(parseFloat(e.target.value))}               
+              style={{
+                padding: "3px",
+                fontSize: "16px",
+                fontWeight: 400,
+              }}/>
+              %rh
+          </div>
+          <div style={{display: "flex", flexDirection: "row", gap: "5px", alignItems: "center", fontSize: "15px", color: "black"}}>
+            연간 강수량
+          <input type="number" value={perp3} onChange={(e) => setPerp3(parseFloat(e.target.value))}               
+              style={{
+                padding: "3px",
+                fontSize: "16px",
+                fontWeight: 400,
+              }}/>
+              mm
+          </div>
+          <div style={{display: "flex", flexDirection: "row", gap: "5px", alignItems: "center", fontSize: "15px", color: "black"}}>
+            평균 풍속
+          <input type="number" value={wind3} onChange={(e) => setWind3(parseFloat(e.target.value))}               
+              style={{
+                padding: "3px",
+                fontSize: "16px",
+                fontWeight: 400,
+              }}/>
+              m/s
+          </div>
+          </div>
+        </label>
         <br />
           <button onClick={handleCalculate} disabled={loading} className="calc-button">
             {loading ? "계산 중..." : "계산하기"}
@@ -94,7 +240,7 @@ const App = () => {
         <div style={{display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"}}>
           <h2>
             결과:{" "}
-            {result.toFixed(2)}회
+            {(result).toFixed(2)}회
           </h2>
           {`${year}년에 ${reg}에서 예측되는 산사태 발생 횟수입니다.`}
         </div>
